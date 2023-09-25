f53230cec34b96de9172bc9c2251f97607c80dd9
Kingsley Kingdon <kinqsleyy@gmail.com>
2023-09-25 14:03:13 +0100

    chaged 'guess' to 'player_guess' to make it easier to read

--- milestone_3.py
+++ milestone_3.py
@@ -3,19 +3,19 @@ import random
 word_list = ['mango', 'apple', 'apricot', 'cherry', 'blueberry']
 word = random.choice(word_list)
 
-def check_guess(guess): # this simply checks if the guess is in the word.
-    guess = guess.lower()
-    if guess in word: 
-        print(f'Good guess! {guess} is in word')
+def check_guess(player_guess): # this simply checks if the guess is in the word.
+    player_guess = player_guess.lower()
+    if player_guess in word: 
+        print(f'Good guess! {player_guess} is in word')
     else:
-        print(f'sorry, {guess} is not in the word.')
+        print(f'sorry, {player_guess} is not in the word.')
 
 def ask_for_input(): # this checks if the input is valid, continuosly asks for input if it isnt valid. but if it is valid, it uses the check_guess function to check if the guess is in the word.
     while True: 
-        guess = input('Please enter one alphabetical charecter: ')
-        if guess.isalpha() == True & len(guess) == 1:
+        player_guess = input('Please enter one alphabetical charecter: ')
+        if player_guess.isalpha() == True & len(player_guess) == 1:
             break
         else:
             print('Invalid letter, please enter a single alphabetical charecter')
-    check_guess(guess)
+    check_guess(player_guess)
     return 
